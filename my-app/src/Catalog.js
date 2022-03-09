934516993e8f385eee8a00bcfb3df72aae0ebc75
Ytaki2134 <tdurand@gaming.tech>
2022-03-09 17:10:01 +0100

    catalog link

--- my-app/src/Catalog.js
+++ my-app/src/Catalog.js
@@ -1,4 +1,5 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import { Row, Col, Form } from 'react-bootstrap';
 /*import Cardsarticles from './Components/Cardsarticles';*/
 import Navigationbar from './Components/Navigationbar';
@@ -58,6 +59,33 @@ function Catalog() {
             </Row >
         </div >
     )
+=======
+import { Row, Col } from 'react-bootstrap';
+import Cardsarticles from './Components/Cardsarticles';
+import Navigationbar from './Components/Navigationbar';
+
+
+function Catalog(props) {
+  console.log(props)
+  return (
+    <div>
+      <Navigationbar />
+      <Row className='nomargin'>
+        <Col className='sidebar' xs={3} sm={2} xl={2} xll={2}>Sidebar</Col>
+        <Col xs={8} sm={10} xl={10} xll={10}>
+          <Row className='space10 catalog'>
+            {
+              props.articles.data &&
+              props.articles.data.map((articles, i) =>
+                <Col xs={12} sm={4} xl={4} xll={4}>
+                  <Cardsarticles articles={articles} />
+                </Col>)}
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  )
+>>>>>>> Stashed changes
 }
 
 export default Catalog;
