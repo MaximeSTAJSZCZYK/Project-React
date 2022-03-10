af86ac552304931009630a5be9cc4e660a79295f
Ytaki2134 <tdurand@gaming.tech>
2022-03-10 22:31:24 +0100

    link articles + catalog

--- my-app/src/Components/Cardsarticles.js
+++ my-app/src/Components/Cardsarticles.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import "./Articles.css";
+import { Link } from 'react-router-dom';
 
 function Cardsarticles(props) {
     return (
     <Card className='margin30'>
+      <Link style={{ textDecoration: 'none' }} to={"/article?ID="+props.articles.id}>
       <Card.Img variant="top" src={'http://localhost:1337' + props.articles.attributes.Picture.data.attributes.url} />
       <Card.Body>
         <Card.Title className='separation'>{props.articles.attributes.Name}</Card.Title>
@@ -12,6 +14,7 @@ function Cardsarticles(props) {
           {props.articles.attributes.Price + '$'}
         </Card.Text>
       </Card.Body>
+      </Link>
     </Card>
   );
 }
