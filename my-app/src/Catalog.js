7697c71aacc09f2c015c6cb8960147e7b7f072d0
Ytaki2134 <tdurand@gaming.tech>
2022-03-10 04:40:24 +0100

    navbar links + functional searchbar

--- my-app/src/Catalog.js
+++ my-app/src/Catalog.js
@@ -1,75 +1,98 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Row, Col, Form, FormControl } from 'react-bootstrap';
 import Cardsarticles from './Components/Cardsarticles';
 import Navigationbar from './Components/Navigationbar';
 
-function Catalog(props) {
-  console.log(props)
-  return (
-    <div>
-      <Navigationbar />
-      <Row className='nomargin'>
-        <Col className='sidebar' xs={3} sm={2} xl={2} xll={2}>
-          <div className='caraname'>Materials :</div>
-          <Form>
-            {['checkbox'].map((type) => (
-              <div key={`default-${type}`} className="mb-3">
-                <Form.Check
-                  type={type}
-                  id={`default-${type}`}
-                  label={`Bois`}
-                />
-              </div>
-            ))}
-          </Form>
-          <div className='caraname'>Type :</div>
-          <Form>
-            {['checkbox'].map((type) => (
-              <div key={`default-${type}`} className="mb-3">
-                <Form.Check
-                  type={type}
-                  id={`default-${type}`}
-                  label={`Bois`}
-                />
-              </div>
-            ))}
-          </Form>
-          <div className='caraname'>Shape :</div>
-          <Form>
-            {['checkbox'].map((type) => (
-              <div key={`default-${type}`} className="mb-3">
-                <Form.Check
-                  type={type}
-                  id={`default-${type}`}
-                  label={`Bois`}
-                />
-              </div>
-            ))}
-          </Form>
+class Catalog extends Component {
 
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-        </Col>
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: ""
+    }
+  }
 
-        <Col xs={8} sm={10} xl={10} xll={10}>
-          <Row className='space10 catalog'>
-            {
-              props.articles.data &&
-              props.articles.data.map((articles, i) =>
-                <Col xs={12} sm={4} xl={4} xll={4}>
-                  <Cardsarticles articles={articles} />
-                </Col>)}
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  )
+  handleChange = (e) => {
+    e.preventDefault()
+    let name = e.target.name
+    this.setState({
+      [name]: e.target.value
+    })
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <Navigationbar />
+        <Row className='nomargin'>
+          <Col className='sidebar' xs={3} sm={2} xl={2} xll={2}>
+            <div className='caraname'>Materials :</div>
+            <Form>
+              {['checkbox'].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                  <Form.Check
+                    type={type}
+                    id={`default-${type}`}
+                    label={`Bois`}
+                  />
+                </div>
+              ))}
+            </Form>
+            <div className='caraname'>Type :</div>
+            <Form>
+              {['checkbox'].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                  <Form.Check
+                    type={type}
+                    id={`default-${type}`}
+                    label={`Bois`}
+                  />
+                </div>
+              ))}
+            </Form>
+            <div className='caraname'>Shape :</div>
+            <Form>
+              {['checkbox'].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                  <Form.Check
+                    type={type}
+                    id={`default-${type}`}
+                    label={`Bois`}
+                  />
+                </div>
+              ))}
+            </Form>
+
+            <Form className="d-flex">
+              <FormControl
+                name="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={this.state.name}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </Form>
+          </Col>
+
+          <Col xs={8} sm={10} xl={10} xll={10}>
+            <Row className='space10 catalog'>
+              {
+                this.props.articles.data &&
+                this.props.articles.data.filter(articles =>
+                  articles.attributes.Name.toLowerCase().includes(this.state.search.toLowerCase()))
+                  .map((articles, i) =>
+                    <Col xs={12} sm={4} xl={4} xll={4}>
+                      <Cardsarticles articles={articles} />
+                    </Col>
+                  )}
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
 export default Catalog;
--- my-app/src/Components/Navigationbar.js
+++ my-app/src/Components/Navigationbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BsFillPersonFill, BsFillBasket2Fill } from "react-icons/bs";
+import { BsFillPersonFill, BsFillBasket2Fill, BsBook } from "react-icons/bs";
 import { ImHome } from "react-icons/im";
 import { Nav, Navbar, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Navigationbar() {
     return <Navbar bg="light" expand="lg">
                 <Container className='navdecor'>
-                    <Button variant="light" href="basket"><ImHome /></Button>
-                    <Navbar.Brand className='Title'href="#home"><img src= 'https://cdn.discordapp.com/attachments/947896766798389331/950376930186756176/logotest.png' alt="Logo" />orteaporte</Navbar.Brand>     
+                    <Button variant="light"><Link to="/"><ImHome /></Link></Button>
+                    <Button variant='light'><Link style={{ textDecoration: 'none' }} to="/"><Navbar.Brand className='Title'><img src= 'https://cdn.discordapp.com/attachments/947896766798389331/950376930186756176/logotest.png' alt="Logo" />orteaporte</Navbar.Brand></Link></Button>     
                     <div className='NavigationBar'><Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Button variant="light" href=""><BsFillPersonFill /></Button>
-                            <Button variant="light" href="basket"><BsFillBasket2Fill /></Button>
+                            <Button variant="light"><Link to=""><BsFillPersonFill /></Link></Button>
+                            <Button variant="light"><Link to="/catalog"><BsBook /></Link></Button>
+                            <Button variant="light"><Link to="/basket"><BsFillBasket2Fill /></Link></Button>
                         </Nav>
                         
                     </Navbar.Collapse>
